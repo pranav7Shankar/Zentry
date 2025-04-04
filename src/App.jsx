4a73d31cb557447f3c28c3228c027eae58107663
pranav7Shankar <pranav7shankar@gmail.com>
2025-04-05 03:30:06 +0530

    First Commit: Completed About

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
+
+const App = () => {
+    return (
+        <main className='relative min-h-screen w-screen overflow-x-hidden'>
+            <Navbar />
+            <Hero />
+            <About/>
+            <Features />
+        </main>
+    )
+}
+export default App

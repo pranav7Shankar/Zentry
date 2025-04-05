9c0eddce7fb0049572697fb1033cf3bca2e58ac7
pranav7Shankar <pranav7shankar@gmail.com>
2025-04-05 16:04:20 +0530

    Finished Story Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Hero />
             <About/>
             <Features />
+            <Story />
         </main>
     )
 }
